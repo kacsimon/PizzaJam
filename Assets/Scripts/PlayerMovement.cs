c7a8a64fee70f0ce8be2c090f82e071148d12e9e
kacsimon <132482344+kacsimon@users.noreply.github.com>
2024-07-09 19:52:41 +0200

    New Player System

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D rb;
     PlayerInput input;
-    [SerializeField] int speed;
+    [SerializeField] float speed;
     void Awake()
     {
-        input = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody2D>();
+        Input input = new Input();
+        input.Player.Enable();
+        input.Player.Move.performed += Input_Move;
+    }
+    void Input_Move(InputAction.CallbackContext obj)
+    {
+        Vector2 inputValue = obj.ReadValue<Vector2>();
+        rb.AddForce(new Vector2(inputValue.x, inputValue.y) * speed * Time.deltaTime, ForceMode2D.Force);
     }
 }
